afc548852106a041c37d4c697973ca49b88978f8
Henrikas Butenas <butenashenrikas@gmail.com>
2022-08-01 15:10:36 +0300

    Access and refresh strategies
    Created strategies for access and refresh tokens

---
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AccessTokenJwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('ACCESS_TOKEN'),
+    });
+  }
+  // Todo: create a type for payload
+  validate(payload: any) {
+    return payload;
+  }
+}
