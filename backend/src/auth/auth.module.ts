9fccb11e26e9c0a28d1813dcb0569e0f030039e6
Henrikas Butenas <butenashenrikas@gmail.com>
2022-08-03 10:23:47 +0300

    Mailmodule import
    Module has to be imported for usage in AuthService

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from 'src/mail/mail.module';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { AccessTokenJwtStrategy, RefreshTokenJwtStrategy } from './strategies';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), MailModule],
   controllers: [AuthController],
   providers: [AuthService, AccessTokenJwtStrategy, RefreshTokenJwtStrategy],
 })
