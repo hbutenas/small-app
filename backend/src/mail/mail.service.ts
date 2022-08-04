39fbd665f25e9abdcb65192b185a3c621295730c
Henrikas Butenas <butenashenrikas@gmail.com>
2022-08-04 21:24:17 +0300

    Mail html
    Baic code formating for ui, to redirect user to the reset password page

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -5,14 +5,18 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 export class MailService {
   constructor(private mailerService: MailerService) {}
 
-  public async sendEmail(requestAuthorEmail: string) {
+  public async sendEmail(requestAuthorEmail: string, hashedToken: string) {
+    const url = 'FE-URL';
     try {
       await this.mailerService.sendMail({
         to: requestAuthorEmail, // Receiver
         from: 'user@outlook.com', // Senders email address
         subject: 'Testing Nest MailerModule ✔', // Subject line
         text: 'welcome', // plaintext body
-        html: '<b>welcome</b>', // HTML body content
+        html: `<p>
+        Click here to reset your password:</br>
+        <a href="${url}/reset-password?token=${hashedToken}">Reset password</a>
+        </p> `, // HTML body content
       });
     } catch (e) {
       throw new InternalServerErrorException('Something went wrong while sending the email... Please try again later');
